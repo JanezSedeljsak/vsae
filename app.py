ac1072cdb0afe8f3991f09367f98a0df3ccf5aeb
JanezSedeljsak <janez.sedeljsak@gmail.com>
2020-04-14 09:46:23 +0200

    .......

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from api.utils.factory import create_app
 from api.utils.config import DevelopmentConfig, ProductionConfig
+from flask_cors import CORS
 import os
 
-if __name__ == '__main__':
-    if os.environ.get('WORK_ENV') == 'PROD':
-        app = create_app(ProductionConfig)
-        app.run(port=5000, host="0.0.0.0", use_reloader=False)
-    else:
-        app = create_app(DevelopmentConfig)
-        app.run(port=6969, host="0.0.0.0", use_reloader=True)
+
+app = create_app(ProductionConfig)
+CORS(app)
